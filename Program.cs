147494950f1116542206739d209f961e03238323
İrem Kürekçi <ismihan.irem.1999@gmail.com>
2023-07-05 20:37:52 +0300

    read json file and parse it line by line as coordinate
    Kaizen Demo Question 2 is done

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Linq;
+
+
+try
+{
+    string jsonFilePath = "lines.json";
+
+    if (File.Exists(jsonFilePath))
+    {
+        string jsonString = File.ReadAllText(jsonFilePath);
+        string json = jsonString;
+
+        JObject jsonObject = JObject.Parse(json);
+
+        JArray linesArray = (JArray)jsonObject["lines"];
+
+        Console.WriteLine("line | text");
+        foreach (JObject lineObject in linesArray)
+        {
+            int lineNumber = (int)lineObject["coordinate"];
+            string lineText = (string)lineObject["description"];
+
+            Console.WriteLine(lineNumber + " " + lineText);
+        }
+    }
+    else
+    {
+        Console.WriteLine("The " + jsonFilePath + " file could not be found.");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An error occurred: " + ex.Message);
+}
+
+
+
